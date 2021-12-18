3cb3e203ff87b68f8aa16631b05d5744ffb4cdf7
unknown <19520447@gm.uit.edu.vn>
2021-12-19 00:33:10 +0700

    upload code

---
+++ app/src/main/java/com/zingmucsicrelax/zingmucsicplayer/DAO/DB.java
@@ -0,0 +1,36 @@
+package com.zingmucsicrelax.zingmucsicplayer.DAO;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+    public  static Connection getConnection(){
+       Connection connection =null;
+       String Url="jdbc:mysql://192.168.1.20/ZingMusicPlayer";
+       String username="root";
+       String password="123456";
+
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(Url,username,password);
+            Log.d("CHECKED","Sucess");
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage()+" ");
+        } catch (ClassNotFoundException e) {
+            Log.d("CHECKED",e.getMessage()+" ");
+        }
+
+
+        return  connection;
+
+    }
+}

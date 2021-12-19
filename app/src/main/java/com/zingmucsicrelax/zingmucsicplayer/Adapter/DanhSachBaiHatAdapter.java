5f0f61e64f07e0486a528edde4195e94d8ac4bde
Nguyễn Văn Tài <80016453+nguyentai090301@users.noreply.github.com>
2021-12-19 21:09:45 +0700

    Upload baihat

---
+++ app/src/main/java/com/zingmucsicrelax/zingmucsicplayer/Adapter/DanhSachBaiHatAdapter.java
@@ -0,0 +1,114 @@
+package com.zingmucsicrelax.zingmucsicplayer.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.zingmucsicrelax.zingmucsicplayer.Controller.SetOnItemClick;
+import com.zingmucsicrelax.zingmucsicplayer.Model.BaiHat;
+import com.zingmucsicrelax.zingmucsicplayer.R;
+import com.zingmucsicrelax.zingmucsicplayer.View.Player.PlayMusicActivity;
+
+import java.util.ArrayList;
+
+public class DanhSachBaiHatAdapter  extends RecyclerView.Adapter<DanhSachBaiHatAdapter.ViewHodler> {
+    private ArrayList<BaiHat> arrayList;
+    private Context context;
+
+    public DanhSachBaiHatAdapter(ArrayList<BaiHat> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHodler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.dong_baihat,parent,false);
+        return new ViewHodler(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHodler holder, int position) {
+
+        Picasso.with(context).load(arrayList.get(position).getHinhanh()).into(holder.hinhanh);
+        holder.txttencasi.setText(arrayList.get(position).getTencasi());
+        holder.txttenbaihat.setText(arrayList.get(position).getTenbaihat());
+        int theloaiid = arrayList.get(position).getTheloai();
+        String tentheloai = "";
+        switch (theloaiid)
+        {
+            case 1:
+            {
+                tentheloai = "Nhạc trẻ";
+                break;
+            }
+            case 2:
+            {
+                tentheloai = "Trữ tình";
+                break;
+            }
+            case 3:
+            {
+                tentheloai = "Kinh dị";
+                break;
+            }
+            case 4:
+            {
+                tentheloai = "Thiếu nhi";
+                break;
+            }
+            default:
+                tentheloai = "Chưa xác định";
+                break;
+        }
+        holder.txttheloai.setText(tentheloai);
+        holder.SetOnItemClick(new SetOnItemClick() {
+            @Override
+            public void SetOnItemClickListener(View view, int pos) {
+                Intent intent = new Intent(context, PlayMusicActivity.class);
+                intent.putExtra("BH",arrayList.get(pos));
+                context.startActivity(intent);
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHodler extends RecyclerView.ViewHolder implements  View.OnClickListener{
+        TextView txttencasi,txttenbaihat,txttheloai;
+        SetOnItemClick setOnItemClick;
+        ImageView hinhanh;
+        public ViewHodler(@NonNull View itemView) {
+            super(itemView);
+            txttenbaihat = itemView.findViewById(R.id.txttenbaihat);
+            txttencasi=itemView.findViewById(R.id.txtencasi);
+            hinhanh=itemView.findViewById(R.id.hinhanh);
+            txttheloai=itemView.findViewById(R.id.txttheloai);
+            itemView.setOnClickListener(this::onClick);
+        }
+        public  void  SetOnItemClick(SetOnItemClick itemClick){
+            this.setOnItemClick = itemClick;
+        }
+
+        @Override
+        public void onClick(View v) {
+           setOnItemClick.SetOnItemClickListener(v,getAdapterPosition());
+        }
+    }
+}
---
+++ app/src/main/java/com/zingmucsicrelax/zingmucsicplayer/Model/BaiHat.java
@@ -0,0 +1,281 @@
+package com.zingmucsicrelax.zingmucsicplayer.Model;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.zingmucsicrelax.zingmucsicplayer.DAO.DB;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class BaiHat implements Serializable {
+    private  int id;
+    private  int id_playlist;
+    private  String tenbaihat;
+    private  String hinhanh;
+    private  String tencasi;
+    private  String url_baihat;
+    private  int theloai;
+    private  String loibaihat;
+    private Connection connection;
+
+
+
+    public BaiHat(int id, String tenbaihat, String hinhanh, String tencasi, String url_baihat, int theloai, String loibaihat) {
+        this.id = id;
+        this.tenbaihat = tenbaihat;
+        this.hinhanh = hinhanh;
+        this.tencasi = tencasi;
+        this.url_baihat = url_baihat;
+        this.theloai = theloai;
+        this.loibaihat = loibaihat;
+    }
+public BaiHat(int id, int id_playlist, String tenbaihat, String hinhanh, String tencasi, String url_baihat, int theloai, String loibaihat) {
+        this.id = id;
+    this.id_playlist = id_playlist;
+        this.tenbaihat = tenbaihat;
+        this.hinhanh = hinhanh;
+        this.tencasi = tencasi;
+        this.url_baihat = url_baihat;
+        this.theloai = theloai;
+        this.loibaihat = loibaihat;
+
+    }
+
+    public int getId_playlist() {
+        return id_playlist;
+    }
+
+    public  BaiHat() {
+       connection = DB.getConnection();
+    }
+    public ArrayList<BaiHat> getDataListBaiHat(){
+
+        ArrayList<BaiHat> arrayList = new ArrayList<>();
+        String SQL="SELECT * FROM baihat";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(new BaiHat(rs.getInt("id"),rs.getString("tenbaihat"),
+                        rs.getString("hinhanh"),
+                        rs.getString("tencasi"),
+                        rs.getString("url_baihat"),
+                        rs.getInt("id_theloai"),
+                        rs.getString("loibaihat")));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+
+
+        return  arrayList;
+
+    }
+    public ArrayList<BaiHat> getDataListBaiHatYeuThich(int iduser){
+
+        ArrayList<BaiHat> arrayList = new ArrayList<>();
+        String SQL="SELECT baihatyeuthich.id,baihat.tenbaihat,baihat.hinhanh,baihat.tencasi," +
+                "                baihat.url_baihat,baihat.id_theloai,baihat.loibaihat FROM baihat,baihatyeuthich" +
+                "                 WHERE baihat.id= baihatyeuthich.id_baihat AND baihatyeuthich.id_user= '"+iduser+"'";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(new BaiHat(rs.getInt("id"),rs.getString("tenbaihat"),
+                        rs.getString("hinhanh"),
+                        rs.getString("tencasi"),
+                        rs.getString("url_baihat"),
+                        rs.getInt("id_theloai"),
+                        rs.getString("loibaihat")));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+
+
+        return  arrayList;
+
+    }
+
+    public ArrayList<String> getDataListBanner(){
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        String SQL="SELECT * FROM banner";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(rs.getString("hinhanh"));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+
+
+        return  arrayList;
+
+    }
+    public String gettentheloai(int id_theloai)
+    {
+        ArrayList<String> arrayList = new ArrayList<>();
+        String tentheloai = "";
+        String SQL="SELECT * FROM THELOAI WHERE THELOAI.ID = "+id_theloai;
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                tentheloai = rs.getString("tentheloai");
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+        return tentheloai;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTenbaihat() {
+        return tenbaihat;
+    }
+
+    public void setTenbaihat(String tenbaihat) {
+        this.tenbaihat = tenbaihat;
+    }
+
+    public String getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(String hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+
+    public String getTencasi() {
+        return tencasi;
+    }
+
+    public void setTencasi(String tencasi) {
+        this.tencasi = tencasi;
+    }
+
+    public String getUrl_baihat() {
+        return url_baihat;
+    }
+
+    public void setUrl_baihat(String url_baihat) {
+        this.url_baihat = url_baihat;
+    }
+
+    public int getTheloai() {
+        return theloai;
+    }
+    public void setTheloai(int theloai) {
+        this.theloai = theloai;
+    }
+
+    public String getLoibaihat() {
+        return loibaihat;
+    }
+
+    public void setLoibaihat(String loibaihat) {
+        this.loibaihat = loibaihat;
+    }
+
+    public void XoaYeuThichBH(int id) {
+        String SQL="DELETE FROM baihatyeuthich WHERE id= '"+id+"'";
+        try {
+            PreparedStatement psm = connection.prepareStatement(SQL);
+            psm.execute();
+            psm.close();
+        } catch (SQLException throwables) {
+         Log.d("CHECKED",throwables.getMessage());
+        }
+    }
+
+    public ArrayList<Playlist> getDataPlayList(int iduser) {
+        ArrayList<Playlist> arrayList = new ArrayList<>();
+        String SQL="SELECT * FROM playlist WHERE id_user = '"+iduser+"'";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(new Playlist(rs.getInt("id"),rs.getString("tenplaylist")));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+
+
+        return  arrayList;
+    }
+
+    public ArrayList<BaiHat> getDataListBaiHatPlayList(int userID, int id) {
+        ArrayList<BaiHat> arrayList = new ArrayList<>();
+        String SQL="SELECT playlist_chitiet.id,playlist_chitiet.id_playlist,baihat.tenbaihat,baihat.hinhanh,baihat.tencasi,\n" +
+                "                              baihat.url_baihat,baihat.id_theloai,baihat.loibaihat FROM baihat,playlist_chitiet,playlist\n" +
+                "                                WHERE baihat.id= playlist_chitiet.id_baihat AND playlist.id_user= '"+userID+"' " +
+                "                AND playlist_chitiet.id_playlist = playlist.id AND playlist_chitiet.id_playlist= '"+id+"'";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(new BaiHat(rs.getInt("id"),rs.getInt("id_playlist"),rs.getString("tenbaihat"),
+                        rs.getString("hinhanh"),
+                        rs.getString("tencasi"),
+                        rs.getString("url_baihat"),
+                        rs.getInt("id_theloai"),
+                        rs.getString("loibaihat")));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+
+
+        return  arrayList;
+    }
+
+    public void XoaPlayList(int id) {
+        String SQL="DELETE FROM playlist_chitiet WHERE id= '"+id+"'";
+        try {
+            PreparedStatement psm = connection.prepareStatement(SQL);
+            psm.execute();
+            psm.close();
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+    }
+
+    public ArrayList<BaiHat> getDataListBaiHatSearch(String name) {
+        ArrayList<BaiHat> arrayList = new ArrayList<>();
+        String SQL="SELECT * FROM baihat WHERE tenbaihat LIKE '%"+name+"%'";
+        try {
+            Statement sm = connection.createStatement();
+            ResultSet rs = sm.executeQuery(SQL);
+            while (rs.next()){
+                arrayList.add(new BaiHat(rs.getInt("id"),rs.getString("tenbaihat"),
+                        rs.getString("hinhanh"),
+                        rs.getString("tencasi"),
+                        rs.getString("url_baihat"),
+                        rs.getInt("id_theloai"),
+                        rs.getString("loibaihat")));
+            }
+        } catch (SQLException throwables) {
+            Log.d("CHECKED",throwables.getMessage());
+        }
+        return  arrayList;
+
+    }
+}
